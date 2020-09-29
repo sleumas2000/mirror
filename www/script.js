82ffcf594e8b9dc765cc7bfa6a1f1d632868e341
Sam Sutherland <samuelasutherland@gmail.com>
2020-09-29 23:05:47 +0100

    Every minute function

--- www/script.js
+++ www/script.js
@@ -1,27 +1,42 @@
-let lastProcessedSecond = -1;
-let lastProcessedDay    = "";
+
+let lastProcessedSecond  = -1;
+let lastProcessedDay     = "";
+let everyMinuteProcessed = false;
 function clockUpdate() {
   let now = new Date();
 
   let s = now.getSeconds().toString().padStart(2, "0")
-  if (s == lastProcessedSecond) {
+
+  // Check if this second has already been processed
+  if (s === lastProcessedSecond) {
     return
   } else {
     lastProcessedSecond = s;
   }
+
   let m = now.getMinutes().toString().padStart(2, "0")
   let h = now.getHours().toString().padStart(2, "0")
   $("#clock-h").text(h);
   $("#clock-m").text(m);
   $("#clock-s").text(s);
-  if (parseInt(s)%2 == 1) {
+  if (parseInt(s)%2 === 1) {
     $("#clock-colon-s, #clock-colon-m").addClass("transparent")
   } else {
     $("#clock-colon-s, #clock-colon-m").removeClass("transparent")
   }
 
+  //Check if this minute has already been processed
+  if (!everyMinuteProcessed && s === "00") {
+    everyMinute();
+    everyMinuteProcessed = true;
+  } else if (s !== "00") {
+    everyMinuteProcessed = false;
+  }
+
   let day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][now.getDay()]
-  if (day == lastProcessedDay) {
+
+  // Check if today has already been processed
+  if (day === lastProcessedDay) {
     return
   } else {
     lastProcessedDay = day;
@@ -35,4 +50,12 @@ function clockUpdate() {
   $("#date-year").text(year)
 }
 
-setInterval(clockUpdate, 200)
+function everyMinute() {
+
+}
+function onLoad() {
+  // Keep the clock up to date
+  setInterval(clockUpdate, 200)
+  // Run the every minute script to initialise the screen
+  everyMinute();
+}
