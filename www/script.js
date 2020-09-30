249faa4873a52d9aaa3f32064317b46ff20902bd
Sam Sutherland <samuelasutherland@gmail.com>
2020-09-30 18:02:10 +0100

    Adding foundations for events,buses,tasks

--- www/script.js
+++ www/script.js
@@ -1,8 +1,9 @@
 
+// Clock update loop
 let lastProcessedSecond  = -1;
 let lastProcessedDay     = "";
 let everyMinuteProcessed = false;
-function clockUpdate() {
+function updateClock() {
   let now = new Date();
 
   let s = now.getSeconds().toString().padStart(2, "0")
@@ -27,6 +28,7 @@ function clockUpdate() {
 
   //Check if this minute has already been processed
   if (!everyMinuteProcessed && s === "00") {
+    // Run the every minute update loop
     everyMinute();
     everyMinuteProcessed = true;
   } else if (s !== "00") {
@@ -50,12 +52,51 @@ function clockUpdate() {
   $("#date-year").text(year)
 }
 
-function everyMinute() {
+let localTasks = [];
+let localEvents = [];
+
+function getTasks() {
+  return new Promise(function(resolve) {
+    resolve([{title:"Do the thing",status:"todo",}])
+  }).then(function(remoteTasks){
+    localTasks = remoteTasks;
+  });
+}
+function getEvents() {
+  return new Promise(function(resolve) {
+    resolve([{title:"Be at the place",start:new Date("2020-09-30T04:00Z"),end:new Date("2020-09-30T04:30Z"),location:"Campus"}])
+  }).then(function(remoteEvents){
+    remoteEvents.sort(function(a,b){
+      return a.start.getTime()-b.start.getTime()
+    });
+    currentEvents = remoteEvents;
+  });
+}
+
+function updateUI() {
 
 }
+
+// Hooks
+
+function everyMinute() {
+  getTasks();
+  getEvents();
+  updateUI();
+}
+
+function onKeypress(key) {
+  updateUI();
+}
+
+// On page load
 function onLoad() {
   // Keep the clock up to date
-  setInterval(clockUpdate, 200)
+  updateClock()
+  setInterval(updateClock, 200)
   // Run the every minute script to initialise the screen
   everyMinute();
 }
+
+// Start
+onLoad()
